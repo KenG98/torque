c94c7e08cdc2c59f1a6db2a7bf99decb74cf131c
Ken Garber <kengarber98@gmail.com>
2016-05-12 23:07:06 -0400

    Bar swings on its own
    And conserves energy!

--- public/page.js
+++ public/page.js
@@ -11,25 +11,33 @@ socket.on('updateData', function(data){
 });
 
 function redraw(){
-	
+
 }
 
 $(document).keydown(function(e) {
     switch(e.which) {
         case 37: // left
-        // socket.emit('position change', [-10, 0]);
-        break;
-		
-		case 38: // up
-		// socket.emit('position change', [0, -10]);
-        break;
-
-        case 40: // down
-        // socket.emit('position change', [0, 10]);
+        socket.emit('move', -1);
         break;
 
         case 39: // right
-        // socket.emit('position change', [10, 0]);
+        socket.emit('move', 1);
+        break;
+
+        default: 
+        return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+});
+
+$(document).keyup(function(e) {
+    switch(e.which) {
+        case 37: // left
+        socket.emit('stop move');
+        break;
+
+        case 39: // right
+        socket.emit('stop move');
         break;
 
         default: 
--- server.js
+++ server.js
@@ -13,14 +13,24 @@ var gravity = 10;
 var bar = {};
 bar.length = 600;
 bar.width = 10;
+bar.mass = bar.length * bar.width * bar.width;
 bar.com = bar.length / 2;
 bar.pivotPoint = 280; 
-bar.angle = 0; // goes +/- 80 degrees
+bar.angle = 0; // goes +/- pi/2 radians
+bar.angularVelocity = 0;
+bar.angularAcceleration = 0;
+bar.torque = 0;
+var moment = 1 / 12 * bar.mass * bar.length * bar.length;
+moment += bar.mass * bar.length * bar.length;
+// moment might be calculated incorrectly... for the purposes of this game.
+bar.moment = moment;
 
 var game = {};
 game.players = players;
 game.bar = bar;
 game.gravity = gravity;
+
+var movingPlayers = {};
 
 server.listen(port, function () {
   console.log('Server listening at port ' + port);
@@ -36,11 +46,52 @@ io.on('connection', function (socket) {
 	console.log('Someone joined the party!');
 
 	socket.id = shortid.generate();
+	game.players[socket.id].position = bar.length / 2;
+	game.players[socket.id].mass = 1000;
 
 	socket.on('disconnect', function(){
     	console.log('Someone left the party.');
+    	delete game.players[socket.id];
 	});
 
-	io.emit('updateDate', game);
+	socket.on('move', function(direction) {
+		movingPlayers[socket.id] = direction;
+	});
+
+	socket.on('stop move', function() {
+		delete movingPlayers[socket.id];
+	});
+
+	io.emit('updateData', game);
 
 });
+
+var recalculate = function() {
+	// calculate torque on the bar
+	// angular acceleration of the bar
+	// angular velocity of the bar
+	// move the damn bar
+	// calculate forces on each block (applied force from player, gravity, friction)
+	// move each damn block.
+
+	var torque = 0;
+	for(block in game.players){
+		var displacement = game.players[block].position - bar.pivotPoint; // dist from pivot -> + - <-
+		var currentTorque = game.players[block].mass * gravity * Math.cos(bar.angle) * displacement;
+		torque += currentTorque;
+	}
+	var barDisplacement = bar.length / 2 - bar.pivotPoint;
+	var barTorque = bar.mass * gravity * Math.cos(bar.angle) * barDisplacement;
+	torque += barTorque;
+	bar.torque = torque;
+	bar.angularAcceleration = bar.torque / bar.moment;
+	bar.angularVelocity += bar.angularAcceleration;
+	bar.angle += bar.angularVelocity;
+	console.log(bar.angle);
+};
+
+setInterval(recalculate, 34);
+
+
+
+
